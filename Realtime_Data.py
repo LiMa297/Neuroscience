125852bdfec029e98e042c906c5ef5017143fe11
EliPreter <118447994+EliPreter@users.noreply.github.com>
2024-06-09 17:35:24 +0200

    getting beta works finally

--- Realtime_Data.py
+++ Realtime_Data.py
@@ -3,7 +3,7 @@ on basis of sample script
 """
 import asyncio
 from idun_guardian_sdk import GuardianClient, FileTypes
-from Play_MP3 import handle_drowsy
+from Play_MP3 import MP3Player
 
 my_api_token = "idun_QN0Cq1f2G3mpJjjblfC_hdW-AwftSG7jBaSpQU-XpHONk6IRXN4x13Yp"
 RECORDING_TIMER = 100000    # 60 * 15  # 15 min
@@ -13,11 +13,15 @@ def print_data(data):
     print(data.message)
 
 
-"""def check_beta(data):
+def check_beta(data):
     # Ensure 'stateless_z_scores' is a non-empty list
-    if data.get('stateless_z_scores'):
+    # print(data.message)
+    # print('hugo')
+    # print(data.message.get('stateless_z_scores'))
+    # print('walter')
+    if data.message.get('stateless_z_scores'):
         # Extract the first dictionary in the list
-        z_scores = data['stateless_z_scores'][0]
+        z_scores = data.message['stateless_z_scores'][0]
 
         # Extract the beta value
         beta = z_scores.get('Beta')
@@ -31,7 +35,7 @@ def print_data(data):
             print("Beta value is not available in the data.")
 
     else:
-        print("stateless_z_scores is empty or not available.")"""
+        print("stateless_z_scores is empty or not available.")
 
 
 if __name__ == "__main__":
@@ -39,7 +43,7 @@ if __name__ == "__main__":
     client.address = asyncio.run(client.search_device())
 
     # subscribe_live_insights(raw_eeg=False, filtered_eeg=True, handler=print_data)
-    client.subscribe_realtime_predictions(fft=True, jaw_clench=False, handler=handle_drowsy)
+    client.subscribe_realtime_predictions(fft=True, jaw_clench=False, handler=check_beta)
 
     asyncio.run(client.start_recording(recording_timer=RECORDING_TIMER))
     rec_id = client.get_recording_id()
